c234c7ebf4a98d8fe6f525a29cd91e3e4b5612c4
murewaabigal <murewaabigal@gmail.com>
2024-10-17 11:03:13 +0100

    Updated

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -7,12 +7,13 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Hotel_Reservation_System.Data;
 using Hotel_Reservation_System.Models;
+using Hotel_Reservation_System.Dto;
 
 namespace Hotel_Reservation_System.Controllers
 {
-    
+    [Route("api/[controller]")]
     [ApiController]
-    
+
     public class UsersController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -23,16 +24,80 @@ namespace Hotel_Reservation_System.Controllers
         }
 
         // GET: Users
+       
+
         [HttpGet]
-        [Route("api/[controller]")]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
-         {
-             if (_context.Users == null)
-             {
-                 return NotFound();
-             }
-             return await _context.Users.ToListAsync();
-         }
+        public IQueryable<UserDto> GetUserdto()
+        {
+            var user = from b in _context.Users
+                       select new UserDto()
+                       {
+                           FirstName = b.FirstName,
+                           LastName = b.LastName,
+                           Email = b.Email,
+                           NIN = b.NIN,
+                           Gender = b.Gender,
+                           PhoneNo = b.PhoneNo,
+                           StateofResidence = b.StateofResidence
+                       };
+
+            return user;
+        }
+
+        [HttpGet("GetUserByEmailAndPassword")]
+        public async Task<ActionResult<UserDto>> GetUserByEmailAndPassword(string email, string password)
+        {
+            // Find user by email
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+           
+
+            // Map to DTO (to avoid exposing sensitive info)
+            var userDto = new UserDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                NIN = user.NIN,
+                Gender = user.Gender,
+                PhoneNo = user.PhoneNo,
+                StateofResidence = user.StateofResidence
+                // Do not return sensitive information like the password
+            };
+
+            return Ok(userDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            if (_context.Users == null)
+            {
+                return Problem("Entity set 'ApplicationDBContext.Users'  is null.");
+            }
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUserByEmailAndPassword), new { email = user.Email, password = user.Password }, user);
+        }
+
+
+        /* public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+          {
+              if (_context.Users == null)
+              {
+                  return NotFound();
+              }
+              return await _context.Users.ToListAsync();
+          }*/
+
+
+
 
 
     }
---
+++ Dto/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_Reservation_System.Dto
+{
+    public class UserDto
+    {
+        public string? FirstName { get; set; }
+       
+        public string? LastName { get; set; }
+      
+        public string? Email { get; set; }
+        
+        public int NIN { get; set; }
+        public string? Gender { get; set; }
+        public int PhoneNo { get; set; }
+        public string? StateofResidence { get; set; }
+    }
+}
